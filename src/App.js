220e8abc786b076d9a048d6d6452033ceec0159a
juan <el@duartte.co>
2021-08-05 23:05:55 +0200

    cambio de orden en scss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import InfoSlider from "./components/InfoSlider";
+
 import "./index.scss";
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
+			<InfoSlider name="bitcoin" />
 			<Main />
 			<Footer />
 		</div>
